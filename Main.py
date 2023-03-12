9dc1220747347992324bfbc3b34c3a3b6d6bb628
drmonroy <drmonroy98@gmail.com>
2023-03-12 10:11:35 -0700

    commit message

--- Main.py
+++ Main.py
@@ -9,21 +9,42 @@ class household():
         self.consumption = consumption
         self.currentWealth = 0
         self.connectedHouseholds = []
-    
-    def AskForHelp():
-    """
-    Ask neighbors for wealth if negative current wealth.
-    """
-
+    def AddConnected(self,newHousehold):
+        """
+        Adds the connected households. 
+        @parameters: newHousehold cannot be in connected already.
+        """
+        assert(not newHousehold in self.connectedHouseholds,"newHousehold already in connected.")
+        self.connectedHouseholds.append(newHousehold)
+    def DeleteConnected(self,oldHousehold):
+        """
+        Deletes the connected households.
+        @parameters: oldHousehold must be in list already.
+        """
+        assert(oldHousehold in self.connectedHouseholds,"newHousehold not in connected.")
+        self.connectedHouseholds.delete(oldHousehold)
+    def AskForHelp(self):
+        """
+        Ask neighbors for wealth if negative current wealth.
+        In the future, might be different strategies for how/what to ask.
+        """
+        for neighbor in self.connectedHouseholds:
+            if neighbor.currentWealth>0:
+                neighbor.currentWealth-=1
+                self.currentWealth+=1
+            if self.CurrentWealth==0:
+                break
+                
     def EndTurn():
-    """
-    Get rid of households with negative current wealth.
-    """
-
-    def BeginTurn():
-    """
-    First, we calculate how much is added or subtracted to the current wealth by endowment minus consumption.
-    """
+        """
+        Get rid of households with negative current wealth.
+        """
+        pass
+    def BeginTurn(self):
+        """
+        First, we calculate how much is added or subtracted to the current wealth by endowment minus consumption.
+        """
+        self.currentWealth = self.endowment-self.consumption
 
 
 
@@ -32,18 +53,19 @@ def LoopBeginTurn():
     """
     Begin turn for all households in the economey
     """
-
+    pass
 def LoopAskForHelp():
     """
     Ask households if they want to ask for help
+    Check if the currentWealth meets some criteria to ask for help.
     """
-
+    pass
 def LoopEndTurn():
     """
     Ends the turn
     Tells households the turn is over
     """
-    
+    pass
 
 
         
--- test.py
+++ test.py
@@ -4,12 +4,30 @@ import Main
 # test for household
 
 def test_EndTurn():
+    pass
 
 def test_BeginTurn():
-
+    TestHousehold = household(2,2)
+    TestHousehold.BeginTurn()
+    expected = 0
+    result = TestHousehold.currentWealth
+    assert(expected==result,"currentWealth is wrong.")
 
 def test_AskForHelp():
     TestHousehold1 = household(3,4)
     TestHousehold2 = household(5,3)
+    TestHousehold1.AddConnected(TestHousehold2)
+    TestHousehold2.AddConnected(TestHousehold1)
+    TestHousehold1.BeginTurn()
+    TestHousehold2.BeginTurn()
+    TestHousehold1.AskForHelp()
+    expected = 0
+    result = TestHousehold1.currentWealth
+    assert(expected == result,"First household not updated correctly.")
+    expected1 = 1
+    result1 = TestHousehold2.currentWealth
+    assert(expected1==result1,"Second household not updated correctly.")
+     
+
 
 # test for functions
